dbac9cec008761c4ae1faa5682a550aea3322bf4
小熊 <xiong_tanyu@163.com>
2023-05-22 15:11:15 +0800

    更新提示，字体加粗

--- miniprogram/app.ts
+++ miniprogram/app.ts
@@ -11,7 +11,7 @@ App<IAppOption>({
     updateManager.onUpdateReady(function () {
       wx.showModal({
         title: '更新提示',
-        content: '新版本已经准备好,是否重启应用？',
+        content: '新版本已准备就绪,是否立刻更新?',
         success(res) {
           if (res.confirm) {
             updateManager.applyUpdate()
